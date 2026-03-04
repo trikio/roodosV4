1eaddb7914dbd9a0b276e0cf2ee0cd3bf18e0d5b
trikio <sk8unk@gmail.com>
2026-03-04 12:44:32 -0300

    test actions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Roodos - Marketplace de Autos</title>
+    <title>Roodos - Marketplace de Autos y Casas</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-50">
